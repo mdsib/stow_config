a8b7807759dc00c8ee8c1d9d39ece67697339705
Massimo Siboldi <mdsiboldi@gmail.com>
2018-03-10 23:16:39 -0800

    start using spacemacs, add readme (finally)

--- dotfiles/.emacs-cfg/lisplike.el
+++
@@ -1,8 +0,0 @@
-;;; parinfer
-;(setq parinfer-extensions '(defaults smart-tab pretty-parens evil smart-yank))
-
-;for evil lispy too
-(add-hook 'lispy-mode-hook (lambda ()
-                             (setq cursor-type 'box)
-                             (evil-set-cursor-color "pink")
-                             (define-key lispy-mode-map "i" 'special-lispy-flow)))
--- dotfiles/.emacs-cfg/theme.el
+++
@@ -1,9 +0,0 @@
-;(load-file "~/.emacs-cfg/my-charcoal-black.el")
-;(require 'color-theme)
-;(color-theme-initialize)
-;(color-theme-alt-charcoal)
-
-(require 'airline-themes)
-(load-theme 'airline-cool)
-
-;(powerline-default-theme)
