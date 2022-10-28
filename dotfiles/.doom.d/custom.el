eaf154f5cf01046efbbce08972459a716bade678
massi <mdsiboldi@gmail.com>
2022-10-28 04:54:10 -0700

    doomed

---
+++ dotfiles/.doom.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(lsp-svelte-plugin-css-enable t)
+ '(warning-suppress-types
+   '((org-capture-after-finalize-hook)
+     (org-capture-after-finalize-hook))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
