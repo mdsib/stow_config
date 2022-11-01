3011a99e65f7b49cc3c61338876761073bcbf98a
massi <mdsiboldi@gmail.com>
2022-10-31 21:05:14 -0700

    another heap of changes
    - options to move or skip all conflicts
    - better documentation
    - style fixes / slightly more POSIX-y
    - more verbose options

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,17 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 shopt -s globstar dotglob
 
 tgt="$HOME"
 dry=0
 verbose=0
+forced_action=""
 
-while getopts dv name
+while getopts dvms name
 do
     case "$name" in
         d) dry=1;;
-        v) verbose=1;;
-        ?) printf "Usage: %s [-d] [-v]\n" "$0"
+        v) verbose=$((verbose + 1));;
+        m) forced_action="m";;
+        s) forced_action="s";;
+        ?) printf "Usage: %s [options]
+   -d     dry run
+   -v     verbose
+   -vv    more verbose
+   -vvv   too verbose
+   -m     move conflicts to <...>.old
+   -s     skip conflicts, keeping original file
+" "$0"; exit 1;;
     esac
 done
 
@@ -21,10 +31,10 @@ if [ ! -r "$STOW_DIR" ] || [ ! "$STOW_PROFILE" ]; then
     printf '\n'
     if [[ "$do_setup" =~ ^[yY]$ ]]; then
         ./setup.sh
-        exit 0;
+        exit 0
     else
         echo "You need to run the setup lol. Or fix something."
-        exit 0;
+        exit 0
     fi
 fi
 
@@ -35,7 +45,7 @@ println() {
 # dry run control
 d() {
     if [ "$dry" -eq 0 ]; then
-        v "Running command: $*"
+        vv "Running command: $*"
         "$@"
     else
         println "DRY RUN: Running command: $*"
@@ -44,56 +54,102 @@ d() {
 
 # verbose output
 v() {
-    if [ "$verbose" -eq 1 ]; then
+    if [ "$verbose" -ge 1 ]; then
+        println "INFO: $*"
+    fi
+}
+
+vv() {
+    if [ "$verbose" -ge 2 ]; then
         println "DEBUG: $*"
     fi
 }
 
+vvv() {
+    if [ "$verbose" -ge 3 ]; then
+        println "NOISE: $*"
+    fi
+}
+
+mk_lnk() {
+    v "Linking ${abs_origin} to ${dst_file}"
+    d ln -s "$abs_origin" "$dst_file"
+}
 
 process() {
-    local answer
-    v "Processing ${1}"
-    dst_file="${tgt}/${1#*/}"
+    local action=""
+    vvv "Processing ${1}"
+
+    local dst_file="${tgt}/${1#*/}"
+    local abs_origin="${STOW_DIR}/${1}"
+
+    # make dir
+    local dst_dir
     dst_dir=$(dirname "$dst_file")
-    abs_origin="${STOW_DIR}/${1}"
     if ! [ -d "$dst_dir" ]; then
         d mkdir -p "$dst_dir"
     fi
-    # now we try to make a symbolic link to the file, and ask what to do if it exists
+
+    # check if we have a conflict (file exists and isn't a symlink to stow_config)
     if [ -f "$dst_file" ] && [ "$(realpath -qe "$dst_file")" != "$abs_origin" ]; then
-        while
-            read -erp "
+        v "Conflict!"
+        if [ "$forced_action" != "" ]; then
+            vv "Forcing action: $forced_action"
+        fi
+        # use the flagged action if there is one
+        action="$forced_action"
+
+        while [ ! "$action" = "m" ] && [ ! "$action" = "s" ]
+        do
+            printf "%s" "
 File ${dst_file} exists! What do you wanna do?
 (V)iew the diff
 (m)ove it to ${dst_file}.old
 (s)kip it
-(d)elete it
-Your choice: " answer
-            case "${answer:-v}" in
+Your choice: "
+            read -r action
+            if [ "$action" = "" ]; then
+                action="v"
+            fi
+            case "${action}" in
                 v)
                     v "Diffing..."
                     less -R < <(println "Left is yours, right is stow_config's";
-                                diff -s --color=always "$dst_file" "$abs_origin");;
-                m)
-                    v "Moving..."
-                    d mv "$dst_file" "${dst_file}.old";;
-                s)
-                    v "Skipping...";;
-                d)
-                    v "Deleting..."
-                    d rm "$dst_file";;
+                                diff -s --color=always "$dst_file" "$abs_origin")
+                    ;;
+                m|s)
+                    ;;
+                *)
+                    println "Invalid choice."
+                    ;;
             esac
-            [ "${answer:-v}" == "v" ]
-        do true; done
+        done
     fi
 
-    if [[ -f "$dst_file" && ! ("$dry" -eq 1 && "$answer" =~ ^[dm]$) ]]; then
-        # already linked or explicitly skipped
-        v "Skipping ${dst_file}"
-    else
-        v "Linking ${abs_origin} to ${dst_file}"
-        d ln -s "$abs_origin" "$dst_file"
-    fi
+    # conflict
+    #   skip: no op
+    #   move: move, then link
+    # no conflict
+    #   file exists: no op
+    #   no file exists: link
+
+    case "${action}" in
+        m)
+            v "Moving ${dst_file} -> ${dst_file}.old"
+            d mv "$dst_file" "${dst_file}.old"
+            mk_lnk
+            ;;
+        s)
+            v "${dst_file} exists. Skipping..."
+            ;;
+        "")
+            if [ -f $dst_file ]; then
+                vvv "${dst_file} already linked. Moving on."
+            else
+                mk_lnk
+            fi
+            ;;
+    esac
 }
 
 # TODO: allow multiple profiles and. figure out a way to combine them. zshrc-os => zshrc-{profile} ?
@@ -103,7 +159,10 @@ do
     if [ -f "$f" ]; then
         process "$f"
     else
-        v "Entering ${f}"
+        vv "Entering ${f}"
     fi
 
 done
+
+println "Done!"
+exit 0
